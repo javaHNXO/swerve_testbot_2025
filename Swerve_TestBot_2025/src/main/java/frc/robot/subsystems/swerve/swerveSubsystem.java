17db2d54e4199eca05158175bf1593845a8c85c5
romashik209 <158328412+romashik209@users.noreply.github.com>
2025-12-10 19:56:06 +0000

    deleted the fuckass swerveDrive cuz we're dumb and probably finished the swerveSubsyski

--- Swerve_TestBot_2025/src/main/java/frc/robot/subsystems/swerve/swerveSubsystem.java
+++ Swerve_TestBot_2025/src/main/java/frc/robot/subsystems/swerve/swerveSubsystem.java
@@ -1,74 +1,72 @@
-package frc.robot.subsystems.swerve;
+package frc.robot;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
-import frc.robot.subsystems.SubsystemManager;
-
-import static edu.wpi.first.units.Units.Meter;
-import static edu.wpi.first.units.Units.Second;
-import static edu.wpi.first.units.Units.Volts;
-
-import java.util.function.DoubleSupplier;
-
-import swervelib.SwerveController;
 import swervelib.SwerveDrive;
-import swervelib.SwerveMath;
-import edu.wpi.first.math.util.MathUtil;
-import com.ctre.phoenix6.hardware.Pigeon2;
+import swervelib.SwerveModule;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveModuleConfiguration;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
 
 public class SwerveSubsystem extends SubsystemBase {
 
     private final SwerveDrive swerveDrive;
     private final Pigeon2 gyro;
+    private SwerveDriveOdometry odometry;
 
-    int frontLeftDriveID  = 1;
-    int frontLeftSteerID  = 2;
-    double frontLeftOffset = 0.0;
-
-    int frontRightDriveID = 3;
-    int frontRightSteerID = 4;
-    double frontRightOffset = 0.0;
-
-    int backLeftDriveID   = 5;
-    int backLeftSteerID   = 6;
-    double backLeftOffset = 0.0;
-
-    int backRightDriveID  = 7;
-    int backRightSteerID  = 8;
-    double backRightOffset = 0.0;
+    private final File swerveJsonDir = new File(Filesystem.getDeployDirectory(), "swerve");  
 
     public SwerveSubsystem() {
 
         gyro = new Pigeon2(0);
-        gyro.setYaw(0);
+        gyro.setYaw(0); 
+        odometry = new SwerveDriveOdometry(swerveDrive.getKinematics(), Rotation2d.fromDegrees(gyro.getYaw()));
 
-        swerveDrive = new SwerveDrive(frontLeftDriveID, frontLeftSteerID, frontLeftOffset,
-        frontRightDriveID, frontRightSteerID, frontRightOffset,
-        backLeftDriveID, backLeftSteerID, backLeftOffset,
-        backRightDriveID, backRightSteerID, backRightOffset,
-        gyro);        
+        try {
+        
+            swerveDrive = new SwerveDrive(swerveConfigFolder);
+
+        } catch (Exception e) {
+            DriverStation.reportWarning("WARNING: Swerve JSON files not found yet.");
+            DriverStation.reportWarning("Place them in: deploy/swerve/");
+            DriverStation.reportWarning("Robot will not drive until JSON is added.");
+        }
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean IsOpenLoop) {
+
+        if (swerveDrive == null) return;  // kogda budut json fayli, udalit nahui
+
+        if (fieldRelative) {
+
+            swerveDrive.driveFieldOriented(translation.getX(), translation.getY(), rotation, gyro.getRotation2d());
+
+        } else {
+
+            swerveDrive.drive(translation.getX(), translation.getY(), rotation);
+        }
     }
 
 
-    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
+    public Command driveCommand(DoubleSupplier x, DoubleSupplier y, DoubleSupplier rot) {
         return run(() -> {
 
-            double vx = translationX.getAsDouble();
-            double vy = translationY.getAsDouble();
-            double omega = angularRotationX.getAsDouble();
+            double vx = x.getAsDouble();
+            double vy = y.getAsDouble();
+            double omega = rot.getAsDouble();
 
             //deadband
-            vx = MathUtil.applyDeadband(vx, 0.05);
-            vy = MathUtil.applyDeadband(vy, 0.05);
-            omega = MathUtil.applyDeadband(omega, 0.05);
+            vx = MathUtil.applyDeadband(x, 0.05);
+            vy = MathUtil.applyDeadband(y, 0.05);
+            omega = MathUtil.applyDeadband(rot, 0.05);
 
             Translation2d translation = SwerveMath.scaleTranslation(new Translation2d(
                 vx * swerveDrive.getMaximumChassisVelocity(),
@@ -76,28 +74,12 @@ public class SwerveSubsystem extends SubsystemBase {
             double omegaRadiansPerSecond = Math.pow(omega, 3) * swerveDrive.getMaximumChassisAngularVelocity();
 
 
-            swerveDrive.drive(translation, 
-                            omegaRadiansPerSecond,
-                            true,
-                            false);
+            drive(translation, omegaRadiansPerSecond, true, false);
         });
-    }
-    
-
-    public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
-        swerveDrive.drive(translation, 
-                        rotation, 
-                        fieldRelative, 
-                        false);
     }
 
     @Override
     public void periodic() {
-        swerveDrive.periodic();
-
-        DoubleSupplier translationX = () -> RobotContainer.driver.getLeftX();
-        DoubleSupplier translationY = () -> -RobotContainer.driver.getLeftY();
-        DoubleSupplier angularRotationX = () -> RobotContainer.driver.getRightX();
 
         Rotation2d heading = Rotation2d.fromDegrees(gyro.getYaw());
 
